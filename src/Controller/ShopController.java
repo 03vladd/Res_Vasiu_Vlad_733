0fe19ff840142bfa109d4843948cb17c11fa2ca9
Vlad <vlad.vasiu@stud.ubbcluj.ro>
2026-02-16 09:49:49 +0300

    Created ShopController and updated Main to follow MVC model

---
+++ src/Controller/ShopController.java
@@ -0,0 +1,45 @@
+package Controller;
+
+import Model.*;
+import Repository.ShopRepository;
+import Service.ShopService;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.Scanner;
+
+
+public class ShopController {
+    private ShopRepository repository;
+    private ShopService service;
+
+    public ShopController() {
+        this.repository = new ShopRepository();
+    }
+
+    public void run() throws IOException {
+        List<Customer> customers = repository.loadCustomers("data/customers.json");
+        List<OrderEvent> events = repository.loadEvents("data/events.json");
+        List<Refund> refunds = repository.loadRefunds("data/refunds.json");
+
+        this.service = new ShopService(customers, events, refunds);
+
+        System.out.println("Customers loaded: " + customers.size());
+        System.out.println("Events loaded: " + events.size());
+        System.out.println("Refunds loaded: " + refunds.size());
+        for (Customer c : customers) {
+            System.out.println(c);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input tier: ");
+        String tier = scanner.nextLine();
+
+        List<Customer> filtered = service.filterByTierAndActive(tier);
+        for (Customer c : filtered) {
+            System.out.println(c);
+        }
+
+        scanner.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Controller.ShopController;
 import Model.*;
 import Repository.ShopRepository;
 
@@ -5,18 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ShopRepository repository = new ShopRepository();
-
-        var customers = repository.loadCustomers("data/customers.json");
-        var events = repository.loadEvents("data/events.json");
-        var refunds = repository.loadRefunds("data/refunds.json");
-
-        System.out.println("Customers loaded: " + customers.size());
-        System.out.println("Events loaded: " + events.size());
-        System.out.println("Refunds loaded: " + refunds.size());
-
-        for (var c : customers) {
-            System.out.println(c);
-        }
+        ShopController controller = new ShopController();
+        controller.run();
     }
 }
