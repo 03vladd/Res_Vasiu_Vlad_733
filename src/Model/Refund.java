95ed31974d665b10081be157cc89ba60cd0d704f
Vlad <vlad.vasiu@stud.ubbcluj.ro>
2026-02-16 09:32:57 +0300

    Created Refund

---
+++ src/Model/Refund.java
@@ -0,0 +1,45 @@
+package Model;
+
+public class Refund {
+    private int id;
+    private int customerid;
+    RefundReason reason;
+    int amount;
+
+    public Refund(int id, int customerid, RefundReason reason, int amount) {
+        this.id = id;
+        this.customerid = customerid;
+        this.reason = reason;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    public RefundReason getReason() {
+        return reason;
+    }
+
+    public void setReason(RefundReason reason) {
+        this.reason = reason;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
