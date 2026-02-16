5c9f029ee157c03ffb89b4585e64aae9a9e4bbfd
Vlad <vlad.vasiu@stud.ubbcluj.ro>
2026-02-16 09:49:25 +0300

    Created ShopService

---
+++ src/Service/ShopService.java
@@ -0,0 +1,24 @@
+package Service;
+
+import Model.*;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ShopService {
+    private List<Customer> customers;
+    private List<OrderEvent> events;
+    private List<Refund> refunds;
+
+    public ShopService(List<Customer> customers, List<OrderEvent> events, List<Refund> refunds) {
+        this.customers = customers;
+        this.events = events;
+        this.refunds = refunds;
+    }
+
+    public List<Customer> filterByTierAndActive(String tier) {
+        return customers.stream()
+                .filter(c -> c.getTier().equals(tier) && c.getStatus() == CustomerStatus.ACTIVE)
+                .collect(Collectors.toList());
+    }
+}
